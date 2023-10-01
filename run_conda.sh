2eee72d97305641bb83b717b3e2ff7036bc3cc98
Camozeen <camozeenclean@gmail.com>
2023-10-01 09:19:21 -0400

    accept environment args and send logs to stdout

--- run_conda.sh
+++ run_conda.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-conda run python main.py
+conda_env=""
+if [ -z "$1" ]; then
+  conda_env=" "
+else
+  conda_env=" -n $1 "
+fi
+
+eval "conda run --live-stream${conda_env}python main.py"
